0aa6782998b2bcc50aaf6c9110c7720dbd9cb859
Bagas Defandi <bagasdefandi12@gmail.com>
2022-08-14 18:22:31 +0700

    Finish social media dashboard with theme switcher

---
+++ social-media-dashboard-with-theme-switcher/script.js
@@ -0,0 +1,52 @@
+const switcher = document.querySelector("#theme");
+
+// MediaQueryList object
+const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+let darkModeState = useDark.matches;
+
+// Checks & unchecks the switcher
+function checkToggle(check) {
+  switcher.checked = check;
+}
+
+// Toggles the "dark-mode" class based on if the media query matches
+function toggleDarkMode(state) {
+  checkToggle(state); // when dark mode on the checkbox is checked
+
+  const hasClass = document.body.classList.contains("dark-mode");
+  
+  if (state) {
+    if (!hasClass) {
+      document.body.classList.add("dark-mode");
+    }
+
+  } else {
+    if (hasClass) {
+      document.body.classList.remove("dark-mode");
+      darkModeState = false;
+    }
+  }
+}
+
+// Sets localStorage state
+function setDarkModeLocalStorage(state) {
+  localStorage.setItem("dark-mode", state);
+}
+
+function switchListener() {
+  darkModeState = !darkModeState;
+  toggleDarkMode(darkModeState);
+  setDarkModeLocalStorage(darkModeState);
+}
+
+// Listen for changes in the OS settings
+useDark.addListener(function(evt) {
+  toggleDarkMode(evt.matches);
+  setDarkModeLocalStorage(evt.matches);
+});
+
+// Initial setting
+toggleDarkMode(localStorage.getItem("dark-mode") == "true");
+
+// Listen for switch change
+switcher.addEventListener("change", switchListener);
